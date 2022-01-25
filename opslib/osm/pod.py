a6f0af284b3163afdc3c6f63e6b494111a2b05ec
Gulsum Atici <gulsumatici@gmail.com>
2022-01-25 14:21:53 +0300

    Update pod.py

--- opslib/osm/pod.py
+++ opslib/osm/pod.py
@@ -60,7 +60,7 @@ class IngressResourceV3Builder:
             r["spec"]["tls"] = self.tls
         return r
 
-    def add_rule(self, hostname: str, service_name, path_type, port, path: str = "/"):
+    def add_rule(self, hostname: str, service_name, path_type: str = "Prefix", port, path: str = "/"):
         # This function only supports one path per rule for simplicity
         self._rules.append(
             {
